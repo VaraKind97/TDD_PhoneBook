54f3fd09f02f8594bec9172f3fffdb440b741113
VaraKind97 <valadiva@mail.ru>
2025-06-10 18:48:41 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,35 +1,30 @@
 package ru.netology;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.TreeMap;
+import java.util.*;
 
-    import java.util.*;
+public class PhoneBook {
+    public static HashMap<String, Long> phoneBook = new HashMap<>();
 
-    public class PhoneBook {
-        public static HashMap<String, Long> phoneBook = new HashMap<>();
-
-        public static int add(String name, long number) {
-            return 0;
-        }
-
-
-        public static String findByNumber(long number) {
-            Optional<String> result = phoneBook.entrySet().stream()
-                    .filter(n -> n.getValue().equals(number))
-                    .map(Map.Entry::getKey)
-                    .findFirst();
-
-            return result.orElse("");
-        }
-
-        public static long findByName(String name) {
-            return phoneBook.get(name);
-        }
-
-        public static void printAllNames() {
-            Map<String, Long> sortedMap = new TreeMap<>(phoneBook);
-            sortedMap.keySet().forEach(System.out::println);
-        }
+    public static int add(String name, long number) {
+        phoneBook.put(name, number);
+        return phoneBook.size();
     }
+
+    public static String findByNumber(long number) {
+        Optional<String> result = phoneBook.entrySet().stream()
+                .filter(n -> n.getValue().equals(number))
+                .map(Map.Entry::getKey)
+                .findFirst();
+
+        return result.orElse("");
+    }
+
+    public static long findByName(String name) {
+        return phoneBook.get(name);
+    }
+
+    public static void printAllNames() {
+        Map<String, Long> sortedMap = new TreeMap<>(phoneBook);
+        sortedMap.keySet().forEach(System.out::println);
+    }
+}
