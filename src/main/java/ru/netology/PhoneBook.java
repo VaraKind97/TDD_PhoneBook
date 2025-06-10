eb845665227739e2f505f334962b0760b033fa82
VaraKind97 <valadiva@mail.ru>
2025-06-10 18:39:17 +0300

    Stub and tests for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,4 +1,35 @@
 package ru.netology;
 
-public class PhoneBook {
-}
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
+
+    import java.util.*;
+
+    public class PhoneBook {
+        public static HashMap<String, Long> phoneBook = new HashMap<>();
+
+        public static int add(String name, long number) {
+            return 0;
+        }
+
+
+        public static String findByNumber(long number) {
+            Optional<String> result = phoneBook.entrySet().stream()
+                    .filter(n -> n.getValue().equals(number))
+                    .map(Map.Entry::getKey)
+                    .findFirst();
+
+            return result.orElse("");
+        }
+
+        public static long findByName(String name) {
+            return phoneBook.get(name);
+        }
+
+        public static void printAllNames() {
+            Map<String, Long> sortedMap = new TreeMap<>(phoneBook);
+            sortedMap.keySet().forEach(System.out::println);
+        }
+    }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,59 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import ru.netology.PhoneBook;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+
+    @RepeatedTest(2)
+    public void testAdd_result() {
+        final String name = "TestName";
+        final long number = 9999999999L;
+        final int original = 1;
+        final int result = PhoneBook.add(name, number);
+        assertEquals(original, result);
+    }
+
+    @Test
+    public void testFindByNumber() {
+        final String original = "TestName";
+        final long number = 9999999999L;
+        PhoneBook.phoneBook.put("TestName", 9999999999L);
+        final String result = PhoneBook.findByNumber(number);
+        assertEquals(original, result);
+    }
+
+    @Test
+    public void testFindByName() {
+        final String name = "TestName";
+        final long original = 9999999999L;
+        final long result = PhoneBook.findByName(name);
+        assertEquals(original, result);
+    }
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    public void testPrintAllNames() {
+        PhoneBook.printAllNames();
+        assertEquals("TestName", outputStreamCaptor.toString().trim());
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
 }
