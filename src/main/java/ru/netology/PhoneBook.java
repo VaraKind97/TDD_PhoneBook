b4a9cd4965f5b012b11e185adaaad62573eedafb
VaraKind97 <valadiva@mail.ru>
2025-06-10 18:49:25 +0300

    Revert "add implemented"
    This reverts commit 54f3fd09f02f8594bec9172f3fffdb440b741113.

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,30 +1,35 @@
 package ru.netology;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.TreeMap;
 
-public class PhoneBook {
-    public static HashMap<String, Long> phoneBook = new HashMap<>();
+    import java.util.*;
 
-    public static int add(String name, long number) {
-        phoneBook.put(name, number);
-        return phoneBook.size();
+    public class PhoneBook {
+        public static HashMap<String, Long> phoneBook = new HashMap<>();
+
+        public static int add(String name, long number) {
+            return 0;
+        }
+
+
+        public static String findByNumber(long number) {
+            Optional<String> result = phoneBook.entrySet().stream()
+                    .filter(n -> n.getValue().equals(number))
+                    .map(Map.Entry::getKey)
+                    .findFirst();
+
+            return result.orElse("");
+        }
+
+        public static long findByName(String name) {
+            return phoneBook.get(name);
+        }
+
+        public static void printAllNames() {
+            Map<String, Long> sortedMap = new TreeMap<>(phoneBook);
+            sortedMap.keySet().forEach(System.out::println);
+        }
     }
-
-    public static String findByNumber(long number) {
-        Optional<String> result = phoneBook.entrySet().stream()
-                .filter(n -> n.getValue().equals(number))
-                .map(Map.Entry::getKey)
-                .findFirst();
-
-        return result.orElse("");
-    }
-
-    public static long findByName(String name) {
-        return phoneBook.get(name);
-    }
-
-    public static void printAllNames() {
-        Map<String, Long> sortedMap = new TreeMap<>(phoneBook);
-        sortedMap.keySet().forEach(System.out::println);
-    }
-}
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,10 +1,12 @@
-import ru.netology.PhoneBook;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import ru.netology.PhoneBook;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PhoneBookTest {
